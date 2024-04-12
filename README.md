0653de1e44544455f6949c72160002422ce3f70e
bpp-nanwu <166486949+bpp-nanwu@users.noreply.github.com>
2024-04-12 09:58:00 +0200

    fixed readm+ robots stuff

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 You will be able to install it from git
 
 ```
-pip install https://github.com/bpp-nanwu/python_module_example.git
+pip install git+https://github.com/bpp-nanwu/python_module_example.git
 ```
 
--- minimuh/core.py
+++ minimuh/core.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 def cow_say(text):
     return f"im cow: {text}"
 
@@ -8,3 +11,8 @@ def car_say(text):
 
 def martin_say(text):
     return f"im martin: {text}"
+
+
+def robot_say(text):
+    robot_id = numpy.random.randint(10000)
+    return f"im robot_{robot_id}: {text}"
